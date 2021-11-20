db609139af1c9f3455bd13f14ef7743ae9faeaf0
Matt <magicmatt455@gmail.com>
2021-11-20 05:01:03 -0800

    Just Completed all the operator tasks

---
+++ main.js
@@ -0,0 +1,36 @@
+//- A function that divides two numbers and returns the result
+//- A function that subtracts two numbers and returns the result
+//- Each function will have 2 parameters, `num1`, `num2`
+//- On the console the prompt will print to the user:
+//- *Please enter your first number* (store that number)
+//- *Please enter your second number* (store that number)
+//- *Please enter the operation to perform: **add**, **sub**, **mul**, **div*** (then store the operation)
+//- Based on the operation entered by the user, you will call one of your 4 functions to perform the correct operation
+//- You will then print to the console: *The result is: [the result]*
+
+
+
+
+
+var readlineSync = require('readline-sync');
+
+//- A function that adds two numbers and returns the result
+let x = 5;         
+let y = 2;        
+let z = x + y;
+
+console.log(x + y)
+
+
+//- A function that multiplies two numbers and returns the result
+let x1 = 5;         
+let y1 = 2;        
+let z1 = x1 * y1;
+
+console.log(x1 * y1)
+
+
+//- A function that divides two numbers and returns the result
+
+console.log(12 / 2);
+console.log(12 / 2);
