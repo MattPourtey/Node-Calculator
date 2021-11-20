868cc341278da5d8ba0047ea64d918409af20f05
Matt <magicmatt455@gmail.com>
2021-11-20 05:37:12 -0800

    removed variables

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-//- A function that divides two numbers and returns the result
 //- A function that subtracts two numbers and returns the result
 //- Each function will have 2 parameters, `num1`, `num2`
 //- On the console the prompt will print to the user:
@@ -10,27 +9,16 @@
 
 
 
-
-
 var readlineSync = require('readline-sync');
 
 //- A function that adds two numbers and returns the result
-let x = 5;         
-let y = 2;        
-let z = x + y;
 
-console.log(x + y)
-
+console.log(5 + 2)
 
 //- A function that multiplies two numbers and returns the result
-let x1 = 5;         
-let y1 = 2;        
-let z1 = x1 * y1;
 
-console.log(x1 * y1)
-
+console.log(5 * 5)
 
 //- A function that divides two numbers and returns the result
 
-console.log(12 / 2);
 console.log(12 / 2);
