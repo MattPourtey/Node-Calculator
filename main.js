0752d396f589b796a87532061ed8207dbe0a5dbb
Matt <magicmatt455@gmail.com>
2021-12-01 07:49:56 -0800

    Addes var getNumbers

--- main.js
+++ main.js
@@ -11,21 +11,59 @@
 
 var readlineSync = require('readline-sync');
 
+
 //- A function that adds two numbers and returns the result
 function sum(num1, num2){
     return num1 + num2
 }
+// Calling the sum function (excute)
+var result_sum = sum(10,5)
+console.log(result_sum)
+
+
+
+
+
 //- A function that multiplies two numbers and returns the result
 function product(num1, num2){
     return num1 * num2
 }
+// Calling the product function (excute)
+var result_product = sum(5,5)
+console.log(result_product)
+
+
+
+
+
 //- A function that divides two numbers and returns the result
 function divide(num1, num2){
     return num1 / num2
 }
 
+// Calling the product function (excute)
+var result_divide = sum(5,25)
+console.log(result_divide)
+
+
+
+
+
 //- A function that subtracts two numbers and returns the result
 function minus(num1, num2){
     return num1 - num2
 }
+// Calling the minus function (excute)
+var result = minus(10,5)
+console.log(result)
+
+
+//- *Please enter your first number* (store that number)
+
+var getNumbers = ""
+
+function logName() {
+    console.log(myName);
+  }
+
 
